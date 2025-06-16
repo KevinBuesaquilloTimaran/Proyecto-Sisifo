186f8fa018e3bf1bc26a45d2feffad3dbf3e4f91
Kevin Buesaquillo <kevin.buesaquillo@campusucc.edu.co>
2025-06-16 18:46:42 -0500

    Se cambio el nombre a hola_mundo.py

--- HolaMundo.py
+++
@@ -1,8 +0,0 @@
-nombre: str = "Tu nombre"
-edad: int = 20
-precio: float = 10000.0
-
-def calcular_IVA(precio: float) -> float:
-    return precio * 1.19  # IVA 19% Colombia
-
-print(calcular_IVA(precio))
